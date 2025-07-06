5c6332c06f87d86f123cf5ffe4e2df491b6a2d09
sun-man-ram <manikantasunkara2653@gmail.com>
2025-07-06 18:27:16 +0530

    connectDb adjusted

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,9 +12,11 @@ app.use(rateLimiter);
 //   next();
 // });
 
-
-connectDB();
 app.use("/api/notes", notesRoutes);
+
+connectDB().then(()=>{
 app.listen(5001, () => {
   console.log("Server started on PORT: 5001");
 });
+});
+
