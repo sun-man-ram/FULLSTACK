24fd54e1060844563e791341aeecb1afcefd2911
sun-man-ram <manikantasunkara2653@gmail.com>
2025-07-06 17:50:48 +0530

    basic middle ware addd

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,13 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 const app = express();
+// middlewares
 app.use(express.json());
+app.use((req,res,next)=>{
+  console.log(`Req method is ${req.method} & Req Url is ${req.url}`);
+  next();
+});
+
 connectDB();
 app.use("/api/notes", notesRoutes);
 app.listen(5001, () => {
