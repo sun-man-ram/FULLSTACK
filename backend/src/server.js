e08f5b13c629da86e3799f35e11a66aea86a07b1
sun-man-ram <manikantasunkara2653@gmail.com>
2025-07-06 18:25:26 +0530

    ratelimit deature added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 const app = express();
 // middlewares
 app.use(express.json());
-app.use((req,res,next)=>{
-  console.log(`Req method is ${req.method} & Req Url is ${req.url}`);
-  next();
-});
+
+app.use(rateLimiter);
+// app.use((req,res,next)=>{
+//   console.log(`Req method is ${req.method} & Req Url is ${req.url}`);
+//   next();
+// });
+
 
 connectDB();
 app.use("/api/notes", notesRoutes);
